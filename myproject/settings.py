65ea4e908f9d2976f5c77bf4bd328cfd3763b4ce
tktktakatuska <blue.mountain.syota@iclouud.com>
2022-11-13 00:39:06 +0900

    My Django Girls app2

--- myproject/settings.py
+++ myproject/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # この行を追加
+    
 ]
 
 ROOT_URLCONF = 'myproject.urls'

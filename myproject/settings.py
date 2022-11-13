938e8c44a7fe94c3c0ec6926372778baec2095e6
tktktakatuska <blue.mountain.syota@iclouud.com>
2022-11-13 23:10:07 +0900

    test

--- myproject/settings.py
+++ myproject/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # この行を追加
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
